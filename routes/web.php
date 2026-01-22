a5d6208daeaa74318f781ba7af68e3c7d7abf527
debbylelycaa <markleecio@gmail.com>
2026-01-23 06:11:07 +0700

    fix:  route dan model product

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,28 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
+use App\Models\Product;
 
-Route::get('/install', function () {
-    Artisan::call('migrate:fresh --seed');
-    return "Database Berhasil Diisi";
+Route::get('/init-toko', function () {
+    try {
+        Artisan::call('migrate:fresh');
+        $migrateLog = Artisan::output();
+
+        Artisan::call('db:seed');
+        $seedLog = Artisan::output();
+
+        $count = Product::count();
+
+        return "
+            <h3>Status Migrasi:</h3> <pre>$migrateLog</pre>
+            <h3>Status Seeder:</h3> <pre>$seedLog</pre>
+            <h3>Jumlah Produk di DB:</h3> <b>$count</b> <br><br>
+            <a href='/'>Kembali ke Beranda</a>
+        ";
+    } catch (\Exception $e) {
+        return "Terjadi Error: " . $e->getMessage();
+    }
 });
-
 Route::get('/', [ProductController::class, 'index'])->name('home');
 Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.show');
 
